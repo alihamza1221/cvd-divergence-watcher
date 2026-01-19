bc29e7e7431654cb11274764bc49f4ef56e9ace2
alihamza1221 <alihamza77065@gmail.com>
2026-01-19 23:08:58 +0500

    allow host

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => ({
     hmr: {
       overlay: false,
     },
+    allowedHosts: ["cvdd-matrix-production.up.railway.app"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
